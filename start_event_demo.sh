5bfa2846f66aab2effd9a3058513d25db0e944e4
Vidhi <vidhi_g1@ee.iitr.ac.in>
2026-09-16 01:24:57 +0530

    Add Foxglove layout and startup script for event demo

---
+++ start_event_demo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Source ROS 2 and your workspace
+source /opt/ros/jazzy/setup.bash
+source ~/husarion_ws/install/setup.bash
+
+# Launch the Panther simulator in the background
+ros2 launch husarion_ugv_gazebo simulation.launch.py &
+SIM_PID=$!
+
+# Give the simulator a few seconds to fully start
+sleep 8
+
+# Launch the Foxglove bridge in the background
+ros2 launch foxglove_bridge foxglove_bridge_launch.xml &
+BRIDGE_PID=$!
+
+echo "Simulator PID: $SIM_PID"
+echo "Foxglove Bridge PID: $BRIDGE_PID"
+echo "Now open https://studio.foxglove.dev/ and connect to ws://localhost:8765"
+
+wait
